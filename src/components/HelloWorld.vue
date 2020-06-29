542d1a68e5baf375237053e9e97e1c6aa19e9940
Md. Emtiaz Zahid <emtiazzahid@gmail.com>
2020-06-29 09:27:12 +0600

    testing process env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1 style="color: red">Texts from process env: {{ process.env }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
